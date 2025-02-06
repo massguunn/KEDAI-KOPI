06f8b98fc1964eb898311dd9cb2d9c6b21b189e8
massguunn <rohadigunawan0@gmail.com>
2025-02-06 18:48:14 +0800

    push project kedai kopi

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di luar aside untuk menghilangkan navbar
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
